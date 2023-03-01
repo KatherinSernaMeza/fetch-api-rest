669e4b56dc82f8cf7879a0dde5d14870b82c9158
Katherin Serna <katherin@Key.localdomain>
2023-02-28 20:22:53 -0800

    #Comment: Handle HTTP errors in React.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import "./App.css";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [imageUrl, setImageUrl] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (isLoading) {
-      // ⬅️ solo hacer la solicitud si isLoading = true
-      fetch("https://dog.ceo/api/breeds/image/random")
-        .then((response) => response.json())
-        .then((dog) => {
-          setImageUrl(dog.message);
-          setIsLoading(false);
-        });
+      async function fetchData() {
+        try {
+          const response = await fetch(
+            "https://dog.ceo/api/breeds/image/random"
+          );
+          if (response.ok) {
+            const dog = await response.json();
+            setImageUrl(dog.message);
+            setError(null);
+            setIsLoading(false);
+          } else {
+            setError("Hubo un error al obtener el perrito");
+          }
+        } catch (error) {
+          setError("No pudimos hacer la solicitud para obtener el perrito");
+        }
+      }
+      fetchData();
     }
-  }, [isLoading]); // ⬅️ ahora este efecto se ejecutará cada vez que cambie este estado
+  }, [isLoading]);
 
   const randomDog = () => {
     setIsLoading(true); // ⬅️ simplemente actualizamos isLoading a true
@@ -26,6 +38,16 @@ function App() {
     return (
       <div className="App">
         <h1>Cargando...</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    // ⬅️ mostramos el error (si es que existe)
+    return (
+      <div className="App">
+        <h1>{error}</h1>
+        <button onClick={randomDog}>Volver a intentarlo</button>
       </div>
     );
   }
